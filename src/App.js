dffeb54d4183b1359e08b6925c6acf15b292ae9f
Fabio Bier <fabio@proxymail.nl>
2018-02-16 01:17:44 +0100

    Added a few more basic components. Nothing fancy yet.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TitleCrawl from './components/title-crawl/title-crawl';
 import Pane from './components/pane/pane';
+import Footer from './components/footer/footer';
 import StubData from './store/content.json';
 
 import './App.css';
@@ -8,21 +9,16 @@ import './App.css';
 class App extends Component {
   state = StubData.content[0];
 
-  // getProps = () => {
-  //   console.log(this.state);
-  //   this.state.components.map((component)=> {
-  //     const titleCrawl = component.titleCrawl[0];
-  //   });
-  // }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">{this.state.title}</h1>
+        <header>
+          <TitleCrawl content={this.state.components[0].titleCrawl} />
         </header>
-        <TitleCrawl content={this.state.components[0].titleCrawl} />
-        <Pane content="Test" />
+        <div className="main">
+          <Pane props={this.state.components[1].aboutMe} />
+        </div>
+        <Footer />
       </div>
     );
   }
--- src/components/pane/pane.js
+++ src/components/pane/pane.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './pane.css';
 
 const Pane = (props) => {
+  let content = props.props[0];
+
   return (
-    <section>
-      <h1 className="visually-hidden">Test</h1>
-      <p>{props.content}</p>
+    <section className="pane">
+      <div className="pane__title">
+        <h1>{content.title}</h1>
+      </div>
+      <div className="pane__content">
+        <article>
+          <p>{content.content}</p>
+        </article>
+      </div>
     </section>
   )
 }
