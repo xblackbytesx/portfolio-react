b50c5828f35931ee127d2e2b238a8b946fde9e27
Fabio Bier <fabio@proxymail.nl>
2019-02-16 11:39:42 +0100

    Optimized animation performance by switching to translate for the title scroll.

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -23,6 +23,13 @@ const Skills = (props) => (
         }
       </ul>
     </li>
+    {/* <meter min="0" low="10" optimum="50" high="90" max="100"></meter>*/}
+
+    <meter value="55.93" min="0" max="120.47" title="GB">
+      <div className="meter-gauge">
+        <span>Disk Usage - 55.93GB out of 120GB</span>
+      </div>
+    </meter>
   </ul>
 )
 
