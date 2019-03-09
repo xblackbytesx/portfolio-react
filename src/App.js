915e039853787a873da73ff26a9ef0ad890b3d43
Fabio Bier <fabio@proxymail.nl>
2019-03-09 11:08:52 +0100

    Added customization toggles to enable user custmization.

--- src/App.js
+++ src/App.js
@@ -14,31 +14,51 @@ import StubData from './store/content.json';
 import './static/css/app.css';
 
 class App extends Component {
-  state = StubData.content[0];
+  state = {
+    content: StubData.content[0],
+    displayTiles: false,
+    darkMode: false
+  };
+
+  toggleDisplayType = () => {
+    this.state.displayTiles
+      ? this.setState({displayTiles: false})
+      : this.setState({displayTiles: true})
+  }
+
+  toggleColorMode = () => {
+    this.state.darkMode
+      ? this.setState({darkMode: false})
+      : this.setState({darkMode: true})
+  }
 
   render() {
     return (
-      <div className="App">
-        <Header props={this.state.components[0].titleCrawl} />
+      <div className={`App display-mode--${this.state.displayTiles ? `tiles` : `cards`} color-mode--${this.state.darkMode ? `dark` : `light`}`}>
+        <div class="customizations">
+          <button onClick={this.toggleDisplayType}>ToggleDisplayMode</button>
+          <button onClick={this.toggleColorMode}>ToggleColorMode</button>
+        </div>
+        <Header props={this.state.content.components[0].titleCrawl} />
         <div className="main">
-          <ArticlePane props={this.state.components[1].aboutMe} />
-          <ArticlePane props={this.state.components[2].myWork} />
+          <ArticlePane props={this.state.content.components[1].aboutMe} />
+          <ArticlePane props={this.state.content.components[2].myWork} />
 
-          <Pane title={this.state.components[4].mySkills[0].title}>
-            <Skills skills={this.state.components[4].mySkills[0].skills} />
+          <Pane title={this.state.content.components[4].mySkills[0].title}>
+            <Skills skills={this.state.content.components[4].mySkills[0].skills} />
           </Pane>
 
-          <Pane title={this.state.components[5].myMusic[0].title}>
+          <Pane title={this.state.content.components[5].myMusic[0].title}>
             <LastFm
-              username={this.state.components[5].myMusic[0].lastFm[0].username}
-              apiKey={this.state.components[5].myMusic[0].lastFm[0].apiKey}
-              tracks={this.state.components[5].myMusic[0].lastFm[0].tracks}
+              username={this.state.content.components[5].myMusic[0].lastFm[0].username}
+              apiKey={this.state.content.components[5].myMusic[0].lastFm[0].apiKey}
+              tracks={this.state.content.components[5].myMusic[0].lastFm[0].tracks}
             />
           </Pane>
 
-          <Pane title={this.state.components[6].testimonials[0].title} className="testimonials">
+          <Pane title={this.state.content.components[6].testimonials[0].title} className="testimonials">
             {
-              this.state.components[6].testimonials[0].testimonials.map((item, index) => {
+              this.state.content.components[6].testimonials[0].testimonials.map((item, index) => {
                 return (
                   <Testimonial
                     key={index}
@@ -53,12 +73,12 @@ class App extends Component {
             }
           </Pane>
 
-          <Pane title={this.state.components[7].contactForm[0].title}>
-            <ContactForm email={this.state.components[7].contactForm[0].email} />
+          <Pane title={this.state.content.components[7].contactForm[0].title}>
+            <ContactForm email={this.state.content.components[7].contactForm[0].email} />
           </Pane>
 
         </div>
-        <Footer props={this.state.components[8].footer} />
+        <Footer props={this.state.content.components[8].footer} />
         <NavBar />
       </div>
     );
